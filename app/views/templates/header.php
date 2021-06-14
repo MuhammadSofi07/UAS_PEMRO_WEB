bf264ad24cea50cca1dd3acfd7501b18da745488
MuhammadSofi07 <61622460+MuhammadSofi07@users.noreply.github.com>
2021-06-14 09:57:25 +0700

    Add files via upload

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,24 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Halaman <?= $data['judul']; ?></title>
-    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootsrap.css">
+  <title>Halaman <?= $data['judul']; ?></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootsrap.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+    .row.content {height: 450px}
+    
+    /* Set gray background color and 100% height */
+    .sidenav {
+      padding-top: 20px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    /* Set black background color, white text and some padding */
+    footer {
+      background-color: #555;
+      color: white;
+      padding: 15px;
+    }
+    
+    /* On small screens, set height to 'auto' for sidenav and grid */
+    @media screen and (max-width: 767px) {
+      .sidenav {
+        height: auto;
+        padding: 15px;
+      }
+      .row.content {height:auto;} 
+    }
+  </style>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class ="container">
-    <a class="navbar-brand" href="<?= BASEURL; ?>">TUGAS PEMRO WEB D</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only"></span></a>
-        <a class="nav-item nav-link" href="<?= BASEURL; ?>/cafe">Cafe</a>
-        <a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
-        </div>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="<?= BASEURL; ?>/home">TUGAS UAS PEMRO WEB</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a class="nav-item nav-link active" href="<?= BASEURL; ?>">Home <span class="sr-only"></span></a>
+        <li><a class="nav-item nav-link" href="<?= BASEURL; ?>/cafe">Cafe</a>
+        <li><a class="nav-item nav-link" href="<?= BASEURL; ?>/about">About</a>
+      </ul>
     </div>
   </div>
 </nav>
+
+</body>
+</html>
