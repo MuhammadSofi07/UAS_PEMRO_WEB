8e707a556f332154f141f8a3f8f55a3296f6fb5f
MuhammadSofi07 <61622460+MuhammadSofi07@users.noreply.github.com>
2021-06-14 08:49:04 +0700

    Add files via upload

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,8 +4,6 @@
         <h1 class="display-4">Welcome To Our Website!</h1>
         <p class="lead">Halo, nama kami <?= $data['nama']; ?>.</p>
         <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
 
 
